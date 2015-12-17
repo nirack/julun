5ba911610e4246a4c57559123404ae7cd9042954
nirack <chjk0376>
2015-12-17 15:10:36 +0800

    ddd

---
+++ widgets/src/main/java/com/julun/widgets/filterbar/Area.java
@@ -0,0 +1,124 @@
+package com.julun.widgets.filterbar;
+
+/**
+ * Created by Administrator on 2015-12-16.
+ */
+public class Area {
+    private String id;
+    private String areaname;
+    private String parentid;
+    private String shortname;
+    private int areacode;
+    private int zipcode;
+    private String pinyin;
+    private String lng;
+    private String lat;
+    private int level;
+    private String position;
+    private int sort;
+    private int is_hot;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAreaname() {
+        return areaname;
+    }
+
+    public void setAreaname(String areaname) {
+        this.areaname = areaname;
+    }
+
+    public String getParentid() {
+        return parentid;
+    }
+
+    public void setParentid(String parentid) {
+        this.parentid = parentid;
+    }
+
+    public String getShortname() {
+        return shortname;
+    }
+
+    public void setShortname(String shortname) {
+        this.shortname = shortname;
+    }
+
+    public int getAreacode() {
+        return areacode;
+    }
+
+    public void setAreacode(int areacode) {
+        this.areacode = areacode;
+    }
+
+    public int getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getPinyin() {
+        return pinyin;
+    }
+
+    public void setPinyin(String pinyin) {
+        this.pinyin = pinyin;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getSort() {
+        return sort;
+    }
+
+    public void setSort(int sort) {
+        this.sort = sort;
+    }
+
+    public int getIs_hot() {
+        return is_hot;
+    }
+
+    public void setIs_hot(int is_hot) {
+        this.is_hot = is_hot;
+    }
+}
